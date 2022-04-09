d70c5a304cb7a30cb34c9693503a503d1bb0e80c
David Stinnett <davidstinnett@icloud.com>
2022-04-09 09:58:25 -0400

    tweaking styling on hero image still

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
       <main
         className="hero"
         style={{
-          background: `url(${hero}) no-repeat center center fixed`,
+          background: `url(${hero}) no-repeat center center`,
           backgroundSize: "cover",
           width: "100%",
           height: "100vh",
