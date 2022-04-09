f6c5864e3b98bceeac286d524cbe8c74ca4a22d8
David Stinnett <davidstinnett@icloud.com>
2022-04-09 11:20:52 -0400

    still experimenting with styling for the hero image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,9 +7,17 @@ import "../index.css";
 
 const Hero = () => {
   return (
-    <div className="heroContainer">
-      <main className="hero">
-        <image src={hero} alt="" />
+    <div className="container">
+      <main
+        className="hero"
+        style={{
+          background: `url(${hero}) no-repeat center center`,
+          backgroundSize: "cover",
+          width: "100%",
+          height: "50rem",
+          position: "relative",
+        }}
+      >
         <HeroText />
       </main>
     </div>
