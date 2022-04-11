bf151a6c12c1a719d9ebb4d8653dc092293152ef
David Stinnett <davidstinnett@icloud.com>
2022-04-11 02:06:24 -0400

    project basically complete

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -13,13 +13,21 @@ const HeroText = () => {
         animateIn="fadeIn"
         animateOnce={true}
         initiallyVisible={false}
-        duration={4}
+        duration={3.5}
       >
         <h4>New Games & Accessories</h4>
+
         <h1>
           <span>Monthly Packages.</span>
           <span>Excitement delivered daily.</span>
         </h1>
+      </ScrollAnimation>
+      <ScrollAnimation
+        animateIn="fadeIn"
+        animateOnce={true}
+        initiallyVisible={false}
+        duration={2.5}
+      >
         <section className="paragraph">
           <p>
             What's the best way to shop for the latest video games and
@@ -27,6 +35,14 @@ const HeroText = () => {
             on your doorstep - every month.
           </p>
         </section>
+      </ScrollAnimation>
+      <ScrollAnimation
+        animateIn="bounceInUp"
+        animateOnce={true}
+        initiallyVisible={false}
+        delay={1}
+        duration={1.7}
+      >
         <Button />
       </ScrollAnimation>
     </main>
